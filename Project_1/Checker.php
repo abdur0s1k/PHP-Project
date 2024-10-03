b6f9c7ca46cf2fb60785149253bcfa5b0112e15f
abdur0s1k <alexalexey914@gmail.com>
2024-10-03 08:56:31 +0300

    Checking columns

--- Project_1/Checker.php
+++ Project_1/Checker.php
@@ -2,7 +2,16 @@
 function is_solved(array $board): int {
 
     for ($i = 0; $i < 3; $i++) {
-
+        
         if ($board[$i][0] === $board[$i][1] && $board[$i][1] === $board[$i][2] && $board[$i][0] !== 0) {
             return $board[$i][0];
         }
+        // Проверка столбцов
+        if ($board[0][$i] === $board[1][$i] && $board[1][$i] === $board[2][$i] && $board[0][$i] !== 0) {
+            return $board[0][$i];
+        }
+    }
+
+    return 0;
+} 
+?>
