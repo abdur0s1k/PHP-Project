f1983a8d0dd8c53b36627918bee7b1123c9d4d22
abdur0s1k <alexalexey914@gmail.com>
2024-10-03 08:57:53 +0300

    Checking the diagonals

--- Project_1/Checker.php
+++ Project_1/Checker.php
@@ -6,10 +6,17 @@ function is_solved(array $board): int {
         if ($board[$i][0] === $board[$i][1] && $board[$i][1] === $board[$i][2] && $board[$i][0] !== 0) {
             return $board[$i][0];
         }
-        // Проверка столбцов
+    
         if ($board[0][$i] === $board[1][$i] && $board[1][$i] === $board[2][$i] && $board[0][$i] !== 0) {
             return $board[0][$i];
         }
+    }
+
+    if ($board[0][0] === $board[1][1] && $board[1][1] === $board[2][2] && $board[0][0] !== 0) {
+        return $board[0][0];
+    }
+    if ($board[0][2] === $board[1][1] && $board[1][1] === $board[2][0] && $board[0][2] !== 0) {
+        return $board[0][2];
     }
 
     return 0;
