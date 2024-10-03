88b1435fe33a537e1750f9d92e9b3d1879cd6640
abdur0s1k <alexalexey914@gmail.com>
2024-10-03 12:20:44 +0300

    encryption

---
+++ 5kyu/Caesar.php
@@ -0,0 +1,29 @@
+<?php
+class CaesarCipher {
+    private $shift;
+
+    public function __construct($shift) {
+        $this->shift = $shift;
+    }
+
+    
+    public function encode($input) {
+        $input = strtoupper($input); 
+        $output = '';
+
+        for ($i = 0; $i < strlen($input); $i++) {
+            $char = $input[$i];
+
+            if (ctype_alpha($char)) {
+                $newChar = chr((ord($char) - ord('A') + $this->shift) % 26 + ord('A'));
+                $output .= $newChar;
+            } else {
+                $output .= $char;
+            }
+        }
+
+        return $output;
+    }
+
+  
+
