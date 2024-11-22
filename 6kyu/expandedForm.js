a20cbb0dd9b4d695e28a4c6993553b43c248fdc9
abdur0s1k <alexalexey914@gmail.com>
2024-11-22 21:53:26 +0300

    expandedForm

--- 6kyu/expandedForm.js
+++ 6kyu/expandedForm.js
@@ -3,13 +3,13 @@ function expandedForm(num) {
     let placeValue = 1;
 
     while (num > 0) {
-        const digit = num % 10; // Получаем последнюю цифру
+        const digit = num % 10; 
         if (digit !== 0) {
             const expandedPart = (digit * placeValue).toString();
-            result = expandedPart + (result === "" ? "" : " + " + result); // Добавляем развернутую часть
+            result = expandedPart + (result === "" ? "" : " + " + result); 
         }
-        num = Math.floor(num / 10); // Убираем последнюю цифру
-        placeValue *= 10; // Переходим к следующему разряду
+        num = Math.floor(num / 10); 
+        placeValue *= 10; 
     }
 
     return result;
